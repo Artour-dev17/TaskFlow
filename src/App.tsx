3235eb004122dfb739f8bc81aa8853d26a2174a9
Artour-dev17 <artur.bg9@gmail.com>
2026-05-20 21:30:46 +0400

    added toggle button to completed tasks and added localstorage for tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,100 @@
 import Header from "./components/Header";
-import './App.css'
 import TaskInput from "./components/TaskInput";
+import { useEffect, useState } from "react";
 
+type Task = {
+    id: number;
+    title: string;
+    completed: boolean;
+}
 function App() {
+
+
+    const [tasks, setTasks] = useState<Task[]>(() =>{
+        const savedTasks = localStorage.getItem("tasks");
+        if (savedTasks){
+            return JSON.parse(savedTasks);
+        }
+        return [];
+    });
+
+    const [filter, setFilter] = useState("all");
+
+    const addTask = (newTask:string)=>{
+        const taskObject = {
+            id: Date.now(),
+            title: newTask,
+            completed: false,
+        };
+
+        setTasks([...tasks, taskObject]);
+    };
+
+    const deleteTask = (taskId : number) => {
+      const filteredTasks = tasks.filter(
+          (task) => task.id !== taskId
+      );
+      setTasks(filteredTasks);
+    };
+
+    const toggleTask = (taskId: number) => {
+        const updatedTask = tasks.map((task) => {
+            if (task.id === taskId){
+                return {
+                    ...task,
+                    completed: !task.completed,
+                };
+            }
+            return task;
+        });
+        setTasks(updatedTask);
+    }
+
+    const filteredTasks = tasks.filter((task) => {
+        if (filter === "active"){
+            return !task.completed;
+        }
+        if (filter === "completed"){
+            return task.completed;
+        }
+        return true;
+    });
+
+    useEffect(()=> {
+       localStorage.setItem(
+           "tasks",
+           JSON.stringify(tasks)
+       );
+    },[tasks]);
 
   return (
       <div>
-        <Header />
-        <TaskInput />
-      </div>
+          <Header/>
+          <TaskInput onAddTask={addTask}/>
 
+          <div>
+              <button onClick={() => setFilter("all")}>All</button>
+
+
+              <button onClick={() => setFilter("active")}>Active</button>
+
+
+              <button onClick={() => setFilter("completed")}>Completed</button>
+          </div>
+          {filteredTasks.map((task) => (
+
+              <div key={task.id}>
+                  <p>{task.completed ? "✅" : "⬜"} {task.title}</p>
+                  <button onClick={() => deleteTask(task.id)}>
+                      Delete
+                  </button>
+                  <button onClick={() => toggleTask(task.id)}>
+                      Toggle
+                  </button>
+
+              </div>
+          ))}
+      </div>
   )
 }
 
--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -1,8 +1,18 @@
 import {useState} from "react";
 
-function TaskInput(){
+type TaskInputProps ={
+    onAddTask: (task: string) => void;
+};
+
+
+function TaskInput({onAddTask}:TaskInputProps){
 
     const [task, setTask] = useState("");
+
+    const handleAddTask = ()=> {
+        onAddTask(task);
+        setTask("");
+    };
 
     return(
         <div>
@@ -11,8 +21,7 @@ function TaskInput(){
                 onChange={(event) => setTask(event.target.value)}
                 placeholder="Enter task..."
             />
-            <p>{task}</p>
-            <button>Add</button>
+            <button onClick={handleAddTask}>Add</button>
         </div>
     )
 }
